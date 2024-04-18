916607780a54f90e2dc2210881fc2307328720f0
Kamal Banjade <kamalbanjade67@gmail.com>
2024-04-18 14:56:42 +0545

    added toaster to main

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client'; // Import createRoot from 'react-dom/client'
+import { ToastContainer } from 'react-toastify'; // Import ToastContainer
+import 'react-toastify/dist/ReactToastify.css'; // Import the CSS for ToastContainer
 import App from './App';
 
 // Replace ReactDOM.render with createRoot
 const root = createRoot(document.getElementById('root')); // Use createRoot
 
-// Render your app inside the root
+// Render your app inside the root along with the ToastContainer
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+    <ToastContainer /> {/* Include the ToastContainer */}
+  </>
 );
