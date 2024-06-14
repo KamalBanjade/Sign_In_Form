43b08db72c3adfbdc0f9f3b3b79a2f6409f93139
Kamal Banjade <kamalbanjade67@gmail.com>
2024-06-15 01:08:38 +0545

    "Updated Dashboard component to handle invalid responses and added testBackend function; added /test endpoint to server and refactored server setup"

--- server/app.js
+++ server/app.js
@@ -3,14 +3,22 @@ const app = express();
 require("./db/conn");
 const router = require("./routes/router");
 const cors = require('cors');
-const cookiParser = require("cookie-parser")
+const cookieParser = require("cookie-parser");
 const port = 8009;
 
 app.use(express.json());
-app.use(cookiParser());
-app.use(cors({ origin: 'https://sign-in-form-rho.vercel.app/' }));
+app.use(cookieParser());
+app.use(cors({ origin: 'https://sign-in-form-rho.vercel.app' }));
 app.use(router);
 
-app.listen(port,()=>{
-    console.log(`server start at port no : ${port}`);
-})
+app.get('/test', (req, res) => {
+  res.json({ message: 'Hello from backend' });
+});
+
+app.listen(port, (err) => {
+  if (err) {
+    console.error('Failed to start server:', err);
+  } else {
+    console.log(`Server started at port: ${port}`);
+  }
+});
