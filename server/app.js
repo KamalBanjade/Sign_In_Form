1027098dd56273a8c02c4f173e41e9c2400e617f
Kamal Banjade <kamalbanjade67@gmail.com>
2024-06-15 01:02:01 +0545

    "Update CORS origin in app.js"

--- server/app.js
+++ server/app.js
@@ -8,7 +8,7 @@ const port = 8009;
 
 app.use(express.json());
 app.use(cookiParser());
-app.use(cors({ origin: 'https://sign-in-form-76b7uudni-kamalbanjades-projects.vercel.app/' }));
+app.use(cors({ origin: 'https://sign-in-form-rho.vercel.app/' }));
 app.use(router);
 
 app.listen(port,()=>{
